f1c4c3153ebf39159c7a51f80d7056acb60e02d0
Miguel Halys <mhalys91@gmail.com>
2016-03-08 09:51:07 +0100

    Eliminacion theme Yeptheme.Noactionbar, pantalla de splash, animacion pulse

---
+++ app/src/main/java/miguel/pruebat2_miguelhalys/SplashActivity.java
@@ -0,0 +1,35 @@
+package miguel.pruebat2_miguelhalys;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+public class SplashActivity extends AppCompatActivity {
+    private TextView titleField,subtitleField;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Animation shake = AnimationUtils.loadAnimation(this, R.anim.pulse_animation);
+        titleField= (TextView) findViewById(R.id.splashTitle);
+        subtitleField= (TextView) findViewById(R.id.splashSubtitle);
+        subtitleField.startAnimation(shake);
+        openApp(true);
+    }
+    private void openApp(boolean locationPermission) {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity
+                        .this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+}
