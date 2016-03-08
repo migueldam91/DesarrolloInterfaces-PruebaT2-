2951d1ad826c60284504fa149e35026e7e6a970f
Miguel Halys <mhalys91@gmail.com>
2016-03-08 09:33:35 +0100

    Metodo logearse a MainActivity

--- app/src/main/java/miguel/pruebat2_miguelhalys/LoginActivity.java
+++ app/src/main/java/miguel/pruebat2_miguelhalys/LoginActivity.java
@@ -1,5 +1,7 @@
 package miguel.pruebat2_miguelhalys;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,14 +10,27 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private TextView titleField,subtitleField;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        titleField=(TextView) findViewById(R.id.title);
+        subtitleField=(TextView) findViewById(R.id.subtitle);
+        setFuente();
+    }
+    private void setFuente() {
+        Typeface font = Typeface.createFromAsset(getApplicationContext().getAssets(), "AmaticSC-Regular.ttf");
+        titleField.setTypeface(font);
+        subtitleField.setTypeface(font);
+    }
+    public void logearse(View v){
+        Intent mainActivity = new Intent(LoginActivity.this,MainActivity.class);
+        startActivity(mainActivity);
     }
 
 }
